3e921c436475e92c347db080bfa6a4499c9b0d9c
Caleb Rieck <calebrieck@github.com>
2023-10-13 23:12:51 -0400

    Remember user through refresh

--- src/Home.js
+++ src/Home.js
@@ -1,41 +1,63 @@
-import React, { useState } from "react";
-import { useNavigate } from 'react-router-dom';
-import './Home.css';
-import { useUser } from './UserContext';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Home.css";
+import { useUser } from "./UserContext";
 import { getAuth } from "firebase/auth";
 import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
 
-
 const Home = () => {
-    const { user, setUser } = useUser();
-    const [isPromptingForName, setIsPromptingForName] = useState(!user.name);
-    const [name, setName] = useState('');
-    const navigate = useNavigate();
+  const { user, setUser } = useUser();
+  const [isPromptingForName, setIsPromptingForName] = useState(null);
+  const [name, setName] = useState("");
+  const navigate = useNavigate();
 
-const handleNameSubmit = async () => {
-        if (name) {
-            const userDocRef = doc(getFirestore(), 'users', getAuth().currentUser.uid);
-            await setDoc(userDocRef, { name: name, email: getAuth().currentUser.email }, { merge: true });
+  useEffect(() => {
+    if (user) {
+      setIsPromptingForName(!user.name);
+    }
+  }, [user]);
 
-            const userDocSnap = await getDoc(userDocRef);
-            setUser(userDocSnap.data());
+  const handleNameSubmit = async () => {
+    if (name) {
+      const userDocRef = doc(
+        getFirestore(),
+        "users",
+        getAuth().currentUser.uid
+      );
+      await setDoc(
+        userDocRef,
+        { name: name, email: getAuth().currentUser.email },
+        { merge: true }
+      );
 
-            setIsPromptingForName(false);
-        }
-    };
+      const userDocSnap = await getDoc(userDocRef);
+      setUser(userDocSnap.data());
 
-    return (
-        <div className="background">
-            {isPromptingForName ? (
-                <div>
-                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Enter your name" />
-                    <button onClick={handleNameSubmit}>Submit</button>
-                </div>
-            ) : (
-                <>Welcome home, {user.name}!</>
-            )}
+      setIsPromptingForName(false);
+    }
+  };
+
+  if (isPromptingForName === null) {
+    return <div>Loading...</div>; // or your preferred loading indicator
+  }
+
+  return (
+    <div className="background">
+      {isPromptingForName ? (
+        <div>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Enter your name"
+          />
+          <button onClick={handleNameSubmit}>Submit</button>
         </div>
-    );
+      ) : (
+        <>Welcome home, {user.name}!</>
+      )}
+    </div>
+  );
 };
 
 export default Home;
--- src/UserContext.js
+++ src/UserContext.js
@@ -1,9 +1,29 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useEffect, useContext } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(getAuth(), async (firebaseUser) => {
+      if (firebaseUser) {
+        const userDocRef = doc(getFirestore(), "users", firebaseUser.uid);
+        const userDocSnap = await getDoc(userDocRef);
+        if (userDocSnap.exists()) {
+          setUser(userDocSnap.data());
+        } else {
+          setUser({ uid: firebaseUser.uid, email: firebaseUser.email });
+        }
+      } else {
+        setUser(null);
+      }
+    });
+
+    return unsubscribe; // Cleanup subscription on unmount
+  }, []);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
