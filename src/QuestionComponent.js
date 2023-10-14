3e0fd26f72954e7630496126e6dbeef8476e885c
Caleb Rieck <calebrieck@github.com>
2023-10-14 14:15:38 -0400

    Store flags in firebase date

--- src/QuestionComponent.js
+++ src/QuestionComponent.js
@@ -1,60 +1,107 @@
-import React, { useState } from 'react';
-import { auth, db } from './firebase';
-import { getFirestore, collection, addDoc } from "firebase/firestore";
-import { useUser } from './UserContext';
+import React, { useState } from "react";
+import { auth, db } from "./firebase";
+import {
+  getFirestore,
+  doc,
+  collection,
+  addDoc,
+  getDoc,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore";
+import { useUser } from "./UserContext";
+import "./QuestionComponent.css";
 
-
-import './QuestionComponent.css';
-
+const defaultFlags = {
+  happy: 0,
+  calm: 0,
+  frustrated: 0,
+  anxious: 0,
+  neutral: 0,
+};
 
 const QuestionComponent = ({ question, index, className, onNext }) => {
-    const [selectedOption, setSelectedOption] = useState(null);
-    const user = useUser();
+  const [selectedOption, setSelectedOption] = useState(null);
+  const user = useUser();
 
-    const handleOptionClick = (option) => {
-        setSelectedOption(option);
-    };
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
+  };
 
-    const handleNextClick = async () => {
-        if (selectedOption) {
-            // Save the data to Firebase here
-            const datePlayed = new Date().toISOString().slice(0,10); // get current date in format YYYY-MM-DD
-            const userQuestionsRef = collection(db, 'users', auth.currentUser.uid, 'PlayedDates', datePlayed, 'Questions');
-            await addDoc(userQuestionsRef, {
-                question: question,
-                answer: selectedOption
-            });
-            onNext();
-        } else {
-            // Handle case where an answer isn't selected if required
-            alert("Please select an option before proceeding.");
-        }
-    };
-    
-    return (
-        <div className={`questionContainer ${className}`}>
-            <div className="questionHeader">
-                <div>
-                    <span> {index+1} / 12</span>
-                    <p>{question.description}</p>
-                </div>
-            </div>
-            <div className="designRegion">
-                {/* Your design content goes here */}
-            </div>
-            <div className="responseButtons">
-            {question.options.map((option, index) => (
-                <button 
-                   key={index} 
-                   className={`responseOption ${selectedOption === option ? 'selected' : ''}`} 
-                   onClick={() => handleOptionClick(option)}>
-                   {option}
-                </button>
-            ))}
-            </div>
-            <button className="doneButton" onClick={handleNextClick}>NEXT</button>
+  const handleNextClick = async () => {
+    if (selectedOption) {
+      const datePlayed = new Date().toISOString().slice(0, 10);
+      const userPlayedDateRef = doc(
+        db,
+        "users",
+        auth.currentUser.uid,
+        "PlayedDates",
+        datePlayed
+      );
+
+      // Check if this date document already exists
+      const dateDoc = await getDoc(userPlayedDateRef);
+      if (!dateDoc.exists()) {
+        // If not, set the default flags for that date
+        await setDoc(userPlayedDateRef, { flags: defaultFlags });
+      }
+
+      // Save the question-answer in a sub-collection called "Responses"
+      const userQuestionsRef = collection(userPlayedDateRef, "Responses");
+      await addDoc(userQuestionsRef, {
+        question: question,
+        answer: selectedOption,
+      });
+
+      // Update the flags based on selected option
+      if (selectedOption.flags && selectedOption.flags.length) {
+        let updates = {};
+        let currentFlags = dateDoc.data()?.flags || {}; // Safe navigation with ?.
+
+        selectedOption.flags.forEach((f) => {
+          if (currentFlags && currentFlags[f] !== undefined) {
+            // Additional check for the flag
+            updates[`flags.${f}`] = currentFlags[f] + 1;
+          } else {
+            updates[`flags.${f}`] = 1; // In case the flag doesn't exist yet
+          }
+        });
+        await updateDoc(userPlayedDateRef, updates);
+      }
+
+      onNext();
+    } else {
+      // Handle case where an answer isn't selected if required
+    }
+  };
+
+  return (
+    <div className={`questionContainer ${className}`}>
+      <div className="questionHeader">
+        <div>
+          <span> {index + 1} / 12</span>
+          <p>{question.description}</p>
         </div>
-    );
-}
+      </div>
+      <div className="designRegion">{/* Your design content goes here */}</div>
+      <div className="responseButtons">
+        {question.options.map((option, index) => (
+          <button
+            key={index}
+            className={`responseOption ${
+              selectedOption === option ? "selected" : ""
+            }`}
+            onClick={() => handleOptionClick(option)}
+          >
+            {option.description}
+          </button>
+        ))}
+      </div>
+      <button className="doneButton" onClick={handleNextClick}>
+        NEXT
+      </button>
+    </div>
+  );
+};
 
 export default QuestionComponent;
--- src/Questions.js
+++ src/Questions.js
@@ -1,13 +1,67 @@
+/*
+
+flags = ["happy", "calm", "frustrated", "anxious", "neutral"]
+
+option = 
+{
+    decription: string
+    flags: [string, string, ...]
+}
+
+question = 
+{
+    description: string,
+    options :
+    [
+        option1,
+        option2,
+    ]
+}
+
+*/
+
 export const questions = [
 
     {
         description: "This is a question",
-        options: ["this is an option", "So is this!"],
+        options: [
+            {
+                description: "this option is flagged happy",
+                flags: ["happy"]
+            },
+            {
+                description: "this option is flagged axious",
+                flags: ["anxious"]
+            },
+        ],
     },
 
     {
         description: "This is a NEW question",
-        options: ["this is a new option", "So is this!"],
+        options: [
+            {
+                description: "this option is flagged calm",
+                flags: ["calm"]
+            },
+            {
+                description: "this option is flagged frustrated",
+                flags: ["frustrated"]
+            },
+        ],
+    },
+
+    {
+        description: "This is a BRAND NEW question",
+        options: [
+            {
+                description: "this option is flagged calm AND neutral",
+                flags: ["calm", "neutral"]
+            },
+            {
+                description: "this option is flagged frustrated AND anxious",
+                flags: ["frustrated", "anxious"]
+            },
+        ],
     },
 
 
