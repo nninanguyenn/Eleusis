135ceabdecc6344a7f8e6fe25a274d25e49f7914
Caleb Rieck <calebrieck@github.com>
2023-10-14 13:10:31 -0400

    Handled storing questions, finishing questions page

--- src/QuestionComponent.js
+++ src/QuestionComponent.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { auth, db } from './firebase';
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { useUser } from './UserContext';
+
+
 import './QuestionComponent.css';
 
+
 const QuestionComponent = ({ question, index, className, onNext }) => {
+    const [selectedOption, setSelectedOption] = useState(null);
+    const user = useUser();
+
+    const handleOptionClick = (option) => {
+        setSelectedOption(option);
+    };
+
+    const handleNextClick = async () => {
+        if (selectedOption) {
+            // Save the data to Firebase here
+            const datePlayed = new Date().toISOString().slice(0,10); // get current date in format YYYY-MM-DD
+            const userQuestionsRef = collection(db, 'users', auth.currentUser.uid, 'PlayedDates', datePlayed, 'Questions');
+            await addDoc(userQuestionsRef, {
+                question: question,
+                answer: selectedOption
+            });
+            onNext();
+        } else {
+            // Handle case where an answer isn't selected if required
+            alert("Please select an option before proceeding.");
+        }
+    };
+    
     return (
         <div className={`questionContainer ${className}`}>
             <div className="questionHeader">
@@ -14,11 +43,16 @@ const QuestionComponent = ({ question, index, className, onNext }) => {
                 {/* Your design content goes here */}
             </div>
             <div className="responseButtons">
-                {question.options.map((option, index) => (
-                    <button key={index} className="responseOption">{option}</button>
-                ))}
+            {question.options.map((option, index) => (
+                <button 
+                   key={index} 
+                   className={`responseOption ${selectedOption === option ? 'selected' : ''}`} 
+                   onClick={() => handleOptionClick(option)}>
+                   {option}
+                </button>
+            ))}
             </div>
-            <button className="doneButton" onClick={onNext}>NEXT</button>
+            <button className="doneButton" onClick={handleNextClick}>NEXT</button>
         </div>
     );
 }
